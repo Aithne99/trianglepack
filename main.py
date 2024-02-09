b05bf78682e59ec7942048ceb24b6727a6347436
Nóra Büki <bukinora@inf.u-szeged.hu>
2024-02-09 21:21:18 +0100

    instances from the 3-dimensional matching reduction

--- main.py
+++ main.py
@@ -105,7 +105,8 @@ def build_cplex_model(jobs: List[job]):
 if __name__ == '__main__':
     #jobsizes = [89, 83, 79, 73, 71, 67, 61, 59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2]
     #jobsizes = [8, 2.9, 2, 2, 8, 2.9, 2, 2, 8, 2.9, 2, 2, 8, 2.9, 2, 2, 8, 2.9, 2, 2, 8, 2.9, 2, 2]
-    jobsizes = [24, 13, 7, 7, 5, 5, 5, 3]
+    Mfakt = 15
+    jobsizes = [467 + Mfakt * 8, 467 + Mfakt * 8, 467 + Mfakt * 8, 156 + Mfakt * 4, 156 + Mfakt * 4, 156 + Mfakt * 4, 129 + Mfakt * 2, 131 + Mfakt * 2, 131 + Mfakt * 2, 86 + Mfakt * 2, 89 + Mfakt * 2, 91 + Mfakt * 2, 78 + Mfakt, 79 + Mfakt, 82 + Mfakt]
     jobsizes.sort(reverse=True)
     joblist = [job(i) for i in jobsizes]
     joblist2 = [job(i) for i in jobsizes]
